c56a341a9d5d514a95b1633cc1ad33c13ac441b2
Karov28 <caroffremi@msn.com>
2018-02-23 14:32:38 +0100

    added /history

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log('VERY FIRST NODE JS SERVER');
+var express = require('express');
+var app = express();
+var path = require('path');
+
+var mainRouter = express.Router();
+
+mainRouter.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/history.json'))
+})
+
+app.use('/history', mainRouter)
+app.listen(1337);
+console.log('1337 is the magic port!');
