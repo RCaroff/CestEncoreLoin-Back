ce36f1a24aff0c87419f3b5748e76f16f0ad0085
Karov28 <caroffremi@msn.com>
2018-02-24 19:08:15 +0100

    linked mongodb database with get / post history

--- server.js
+++ server.js
@@ -1,13 +1,77 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var port = process.env.PORT || 1337;
 
-var mainRouter = express.Router();
+var Destination = require('./app/models/destination')
 
-mainRouter.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/history.json'))
+var apiRouter = express.Router();
+mongoose.connect('mongodb://localhost:27017/cel');
+
+apiRouter.use(function(req, res, next) {
+  console.log('Someone just connected');
+  next()
 })
 
-app.use('/history', mainRouter)
-app.listen(1337);
-console.log('1337 is the magic port!');
+apiRouter.get('/', function(req, res) {
+  res.json({
+    message: 'Welcome to my API !'
+  })
+})
+
+
+apiRouter.route('/history')
+.post(function(req, res) {
+  var destination = new Destination()
+  console.log(`req body : ${JSON.stringify(req.body)}`);
+  destination.address = req.body.params.address
+  destination.save(function(err) {
+    if (err) {
+      if (err.code == 11000) {
+        return res.json({
+          success: false,
+          message: 'This destination already exists'
+        })
+      } else {
+        return res.send(err)
+      }
+    }
+
+    res.json({
+      message: 'Destination saved'
+    })
+  })
+})
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, \Authorization')
+  next();
+});
+
+app.use(morgan('dev'))
+
+
+apiRouter.route('/history')
+.get(function(req, res) {
+  var destinations = Destination.find(function(error, dest) {
+    console.log(`destinations fetched : ${dest}`);
+    var toReturn = dest.filter((d, index) => {
+      return typeof d.address !== 'undefined'
+    })
+    res.json({
+      history:toReturn
+    })
+  })
+
+})
+
+app.use('/api', apiRouter)
+app.listen(port);
+console.log(`${port} is the magic port!`);
